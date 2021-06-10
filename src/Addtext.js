e70ccd24e7ab567dd527c40cdd62c7fce39ae42c
Natalia Trzeciak <trzeciak.natalia@icloud.com>
2021-06-10 20:39:42 +0200

    Next comit

--- src/Addtext.js
+++ src/Addtext.js
@@ -24,7 +24,7 @@ export default function Addtext() {
 
   const onChange = (event) => {
     event.preventDefault();
-    setUserInput(() => event.target.value)
+    setUserInput(() => event.target.value);
 }
   
    const emptyInput = (event) => {
